834b5ee0db94605cf89cfcf08b3166aa5b9f6b1b
Rick Kozak <rick@rtbsoft.com>
2017-06-09 15:03:16 +0000

    added comments and non-jquery approach

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,7 @@
+//since we'll be creating a mongoose model for our post, we need to include the mongoose module
 var mongoose = require('mongoose');
 
+//we're building a POST object model in mongoose that we'll use elsewhere
 module.exports = mongoose.model('Post', {
    userId: String, //_id from the user table
    image: String, //url to image file
